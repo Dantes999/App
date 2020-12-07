354c783dad5a8ce2565e11444f2b40a498fa52d2
Paula Schlegel <paula.schlegel@stud.htwk-leipzig.de>
2020-12-07 10:56:42 +0100

    Add Navigation to Recipeslist

--- app/src/main/java/com/example/myapplication/ui/recipes/RecipesFragment.kt
+++ app/src/main/java/com/example/myapplication/ui/recipes/RecipesFragment.kt
@@ -1,14 +1,18 @@
 package com.example.myapplication.ui.recipes
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
+import androidx.cardview.widget.CardView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.example.myapplication.R
+import com.example.myapplication.RecipesList
+import kotlinx.android.synthetic.main.activity_recipes_list.*
+import kotlinx.android.synthetic.main.fragment_recipes.*
+
 
 class RecipesFragment : Fragment() {
 
@@ -22,6 +26,13 @@ class RecipesFragment : Fragment() {
         recipesViewModel =
             ViewModelProvider(this).get(RecipesViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_recipes, container, false)
+
+        val card: CardView = root.findViewById(R.id.sourdough)
+        card.setOnClickListener(View.OnClickListener {
+            val intent = Intent(activity, RecipesList::class.java)
+            startActivity(intent)
+        })
+
         return root
     }
 }
